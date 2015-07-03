8afa748d5d6f91e6c6e93756b10be03b2373ca49
ashokg <ashok.gudur@gmail.com>
2015-07-03 17:43:42 +0530

    Added database logic to read from SQL server

--- WebApiSample/WebApiSample/Controllers/ProductsController.cs
+++ WebApiSample/WebApiSample/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -10,28 +12,63 @@ namespace WebApiSample.Controllers
 {
     public class ProductsController : ApiController
     {
-        Product[] products = new Product[]
-        {
-            new Product { Id = 1, Name = "The Incredible Hulk", Category = "Movies", Price = 200},
-            new Product { Id = 2, Name = "Pepsi 200 ML", Category = "Beverages", Price = 15},
-            new Product { Id = 3, Name = "MI Redmi 2",  Category = "Mobiles", Price = 6999 },
-            new Product { Id = 4, Name = "Redmi MI 4i",  Category = "Mobiles", Price = 14999 },
-            new Product { Id = 5, Name = "Sony Vaio Laptop",  Category = "Laptops", Price = 120000 },
-            new Product { Id = 6, Name = "Lenevo Thinkpad T440p",  Category = "Laptops", Price = 155000 }
-        };
+        Product[] _products = null;
 
         public IEnumerable<Product> GetAllProducts()
         {
-            return products;
+            readProductsFromSqlDb();
+            return _products;
         }
 
         public IHttpActionResult GetProduct(int id)
         {
-            var product = products.FirstOrDefault(p => p.Id == id);
+            var con = openConnection();
+            var cmd = con.CreateCommand();
+            cmd.CommandText = string.Format("select * from products where id = {0}", id);
+            var rdr = cmd.ExecuteReader();
+            Product product = null;
+            if (rdr.Read())
+                product = createProduct(rdr);
+
+            con.Close();
+
             if (product == null)
                 return NotFound();
-
+            
             return Ok(product);
+        }
+
+        private Product[] readProductsFromSqlDb()
+        {
+            var con = openConnection();
+            var cmd = con.CreateCommand();
+            cmd.CommandText = "select * from products";
+            var rdr = cmd.ExecuteReader();
+            List<Product> products = new List<Product>();
+            while (rdr.Read())
+                products.Add(createProduct(rdr));
+
+            con.Close();
+            return products.ToArray();
+        }
+
+        private IDbConnection openConnection()
+        {
+            var con = new SqlConnection(@"Server=68ae607d-8602-4a6a-98ca-a4ca00b9cc75.sqlserver.sequelizer.com;Database=db68ae607d86024a6a98caa4ca00b9cc75;User ID=zodlhhlmghrqjfop;Password=safPsqQT2yMa22HZDXViJSfLZScBCN2aVX6cqfiMZwH2MALvRXhGyzbH4BWHnopm;");
+            con.Open();
+            return con;
+        }
+
+        private Product createProduct(IDataReader rdr)
+        {
+            return new Product
+            {
+                Id = Convert.ToInt32(rdr["id"]),
+                Name = rdr["name"].ToString(),
+                Category = rdr["category"].ToString(),
+                Price = Convert.ToDecimal(rdr["price"])
+            };
+
         }
     }
 }
