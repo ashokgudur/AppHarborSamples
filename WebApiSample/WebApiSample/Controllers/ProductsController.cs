59c62a31d2b8828192aeba486fb3e377a53cff71
ashokg <ashok.gudur@gmail.com>
2015-07-03 16:06:15 +0530

    Initial Comit

---
+++ WebApiSample/WebApiSample/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiSample.Models;
+
+namespace WebApiSample.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "The Incredible Hulk", Category = "Movies", Price = 200},
+            new Product { Id = 2, Name = "Pepsi 200 ML", Category = "Beverages", Price = 15},
+            new Product { Id = 3, Name = "MI Redmi 2",  Category = "Mobiles", Price = 6999 }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+    }
+}
