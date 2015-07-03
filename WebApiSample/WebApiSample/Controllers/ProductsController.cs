9bb2de595982079eea51917c677423dc4a9e0172
ashokg <ashok.gudur@gmail.com>
2015-07-03 16:43:23 +0530

    Added few more products to product samples

--- WebApiSample/WebApiSample/Controllers/ProductsController.cs
+++ WebApiSample/WebApiSample/Controllers/ProductsController.cs
@@ -15,7 +15,9 @@ namespace WebApiSample.Controllers
             new Product { Id = 1, Name = "The Incredible Hulk", Category = "Movies", Price = 200},
             new Product { Id = 2, Name = "Pepsi 200 ML", Category = "Beverages", Price = 15},
             new Product { Id = 3, Name = "MI Redmi 2",  Category = "Mobiles", Price = 6999 },
-            new Product { Id = 4, Name = "Redmi MI 4i",  Category = "Mobiles", Price = 14999 }
+            new Product { Id = 4, Name = "Redmi MI 4i",  Category = "Mobiles", Price = 14999 },
+            new Product { Id = 5, Name = "Sony Vaio Laptop",  Category = "Laptops", Price = 120000 },
+            new Product { Id = 6, Name = "Lenevo Thinkpad T440p",  Category = "Laptops", Price = 155000 }
         };
 
         public IEnumerable<Product> GetAllProducts()
